d48ad2b50b40d917dcc486fcef403ff8b46107ae
somukhan <mhsomrat154@gmail.com>
2024-01-28 02:12:23 +0600

    completed Data Filter

--- src/product/models.py
+++ src/product/models.py
@@ -50,5 +50,5 @@ class ProductVariantPrice(TimeStampMixin):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.product.title
-        # return f"{self.product_variant_one} + {self.product_variant_two} + {self.product_variant_three}"
+        # return self.product.title
+        return f"{self.product_variant_one} + {self.product_variant_two} + {self.product_variant_three}"
