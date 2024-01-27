1a00853d6a4584367b16aa9c514d03b4d4584e67
somukhan <mhsomrat154@gmail.com>
2024-01-27 21:50:46 +0600

    completed product list page with pagination

--- src/product/models.py
+++ src/product/models.py
@@ -8,22 +8,34 @@ class Variant(TimeStampMixin):
     description = models.TextField()
     active = models.BooleanField(default=True)
 
+    def __str__(self):
+        return self.title
+
 
 class Product(TimeStampMixin):
     title = models.CharField(max_length=255)
     sku = models.SlugField(max_length=255, unique=True)
     description = models.TextField()
 
+    def __str__(self):
+        return self.title
+
 
 class ProductImage(TimeStampMixin):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     file_path = models.URLField()
+
+    def __str__(self):
+        return self.product.title
 
 
 class ProductVariant(TimeStampMixin):
     variant_title = models.CharField(max_length=255)
     variant = models.ForeignKey(Variant, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.variant_title
 
 
 class ProductVariantPrice(TimeStampMixin):
@@ -36,3 +48,7 @@ class ProductVariantPrice(TimeStampMixin):
     price = models.FloatField()
     stock = models.FloatField()
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.product.title
+        # return f"{self.product_variant_one} + {self.product_variant_two} + {self.product_variant_three}"
--- src/product/views/product.py
+++ src/product/views/product.py
@@ -1,6 +1,9 @@
+from typing import Any
+from django.db.models.query import QuerySet
+from django.http import HttpRequest, HttpResponse
 from django.views import generic
 
-from product.models import Variant
+from product.models import Variant, Product
 
 
 class CreateProductView(generic.TemplateView):
@@ -12,3 +15,9 @@ class CreateProductView(generic.TemplateView):
         context['product'] = True
         context['variants'] = list(variants.all())
         return context
+
+
+class ListProductView(generic.ListView):
+    model = Product
+    template_name = 'products/list.html'
+    paginate_by = 2
